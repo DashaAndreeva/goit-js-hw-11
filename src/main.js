56d1cc8b1e224e577c0b283869b524d4c44ea6a3
DashaAndreeva <143691908+DashaAndreeva@users.noreply.github.com>
2024-02-11 20:00:24 +0200

    upd-2x

--- src/main.js
+++ src/main.js
@@ -2,18 +2,11 @@
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const refs = {
-  form: document.querySelector('.form'),
-  input: document.querySelector('.input-search'),
-  button: document.querySelector('.button'),
-  gallery: document.querySelector('.gallery'),
-  loader: document.querySelector('.loader'),
-};
-
-let lightBox;
+import { getPhotosByRequest } from './js/pixabay-api';
+import { renderPhotos } from './js/render-function';
+import { refs } from './js/render-function';
 
 refs.loader.style.display = 'none';
 
@@ -50,50 +43,6 @@ function onFormSubmit(e) {
     });
 
   e.target.reset();
-}
-
-function getPhotosByRequest(userRequest) {
-  const API_KEY = '42307458-71f98d2a331f0bd294b8fc1ec';
-
-  const BASE_URL = 'https://pixabay.com';
-  const END_POINTS = '/api/';
-  const PARAMS = `?key=${API_KEY}&q=${userRequest}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-  const url = BASE_URL + END_POINTS + PARAMS;
-
-  return fetch(url).then(res => res.json());
-}
-
-function photosTemplate(photos) {
-  return photos
-    .map(data => {
-      return `
-    <li class="gallery-item"><a href="${data.largeImageURL}">
-          <img class="gallery-image" src="${data.webformatURL}" alt="${data.tags}"></a>
-          <div class="info">
-          <p> <span class="info-text">Likes</span> <br/> ${data.likes}</p>
-          <p><span class="info-text">Views</span> <br/> ${data.views}</p>
-          <p><span class="info-text">Comments</span> <br/> ${data.comments}</p>
-          <p><span class="info-text">Downloads</span> <br/> ${data.downloads}</p>
-          </div>
-          </li>
-    `;
-    })
-    .join('');
-}
-
-function renderPhotos(photos) {
-  const galleryMarkup = photosTemplate(photos);
-  refs.gallery.innerHTML = galleryMarkup;
-
-  if (typeof lightBox !== 'undefined') {
-    lightBox.refresh();
-  } else {
-    lightBox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
-  }
 }
 
 function clearGallery() {
